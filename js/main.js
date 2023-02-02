65a127f82a299309aaf7964d983ea41280e17acd
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-02-02 18:16:48 +0100

    funzionamento raggiunto, aggiustare codice

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 // Declaration
 const play = document.querySelector("#gridgen");                         //button click to create the grid
 const container_bottom = document.querySelector(".container_bottom");       //bottom part of container
-const gridEl = document.querySelector(".grid");  //grid element
-let gridGenerated = false;
+let bombPlacement = [];                                                     //bomb placement var
+let cellSelectedCounter = 0, bombfind = false;                                        // number of cells selected before game over
 
 
 
 //button click generate the grid or reset it
 play.addEventListener("click",
     function () {
-        container_bottom.classList.toggle("d-none");
-        if (!container_bottom.classList.contains("d-none")) {  //if the grid there aren't 
-            generateGrid();                           // generate it
+        const gridEl = document.querySelector(".grid");              //grid element declaration
+        const difficulty = document.querySelector("#difficulty");   //difficulty element declaration
+
+        gridEl.classList.toggle("d-none"); //insert and remove grid visibility
+        if (!gridEl.classList.contains("d-none")) {  //if the grid there aren't 
+            let rangemax = generateGrid(gridEl, difficulty);                           // generate it
+            generateBomb(rangemax);          //bomb generating and placement
+
+
+            for (let i = 0; i < 16; i++) {
+                console.log(bombPlacement[i]);
+            }
         } else {                                            //else reset
-            resetGrid();
+            resetGrid(gridEl);
         }
     }
 );
@@ -29,30 +38,74 @@ play.addEventListener("click",
  *****************************/
 
 
-function generateGrid() {
+function generateGrid(gridEl, difficulty) {
 
-    for (let i = 0; i < 100; i++) {
+    cellsNumber = [100, 81, 49];
+    cellDivision = ["easy", "normal", "hard"];
+
+
+    for (let i = 0; i < cellsNumber[difficulty.value - 1]; i++) {
 
         let squareEl = document.createElement("div");
-        squareEl.classList.add("square");
+        squareEl.classList.add(`square`);
+        squareEl.classList.add(`${cellDivision[difficulty.value - 1]}`);
 
-        // gridEl.innerHTML += `<div class="square">${i + 1}</div>`;
-        // squareEl = document.querySelector(".square");
+
 
         squareEl.addEventListener("click",
             function () {
-                this.classList.toggle("bgGreen");
+
+                //bomb presence control
+                for (let i = 0; i < 16; i++) {
+
+                    if (this.value == bombPlacement[i]) {
+                        this.classList.add("game_over");
+                        bombfind = true;
+                        alert(`Hai perso, hai conquistato ${cellSelectedCounter} celle`);
+                    }
+
+                }
+
+                // se la bomba non è stata trovata
+                if (bombfind == false) {
+                    this.classList.add("active");
+                    cellSelectedCounter += 1;
+                }
+                bombfind = false;//reset bombfind
+
                 console.log(this.classList);
             }
         );
 
         gridEl.append(squareEl);
-        squareEl.innerHTML = `${i + 1}`;
+        squareEl.value = `${i + 1}`; // value assign
+        squareEl.innerHTML = squareEl.value;   // value in html
     }
+
+    return cellsNumber[difficulty.value - 1];
 }
 
-function resetGrid() {
-    const gridEl = document.querySelector(".grid");  //grid element
+
+function resetGrid(gridEl) {
     gridEl.innerHTML = ``;
 }
+
+
+function generateBomb(rangemax) {
+
+    for (let i = 0; i < 16; i++) {
+        bombPlacement[i] = Math.floor(Math.random() * rangemax) + 1;
+
+        //sostituire dopo con un algoritmo migliore
+        for (let j = 0; j < i; j++) {  // if in the precedent index are present a double presence of a number
+            if (bombPlacement[i] == bombPlacement[j]) {
+                bombPlacement[i] = Math.floor(Math.random() * rangemax) + 1;  //replace it
+            }
+        }
+
+
+    }
+
+}
+
 
